920d724adb092b098c21edc0f00192e76eb24d38
Fume Uvwie <25962091+fumzy123@users.noreply.github.com>
2025-08-04 23:04:26 -0230

    refactored config to its own file for separation of concern principle

---
+++ src/config/app.config.js
@@ -0,0 +1,15 @@
+// Configuration Constant single source of truth
+
+export const CONFIG = {
+  canvas: {
+    selector: 'canvas.webgl',
+    width: 800,
+    height: 600,
+  },
+  camera: {
+    fov: 75,
+    near: 0.1,
+    far: 1000,
+    position: { x: 0, y: 0, z: 2 },
+  },
+};
--- src/script.js
+++ src/script.js
@@ -1,22 +1,6 @@
 import * as THREE from 'three';
 import { gsap } from 'gsap';
-
-// ============================================================================
-// CONFIGURATION
-// ============================================================================
-const CONFIG = {
-  canvas: {
-    selector: 'canvas.webgl',
-    width: 800,
-    height: 600,
-  },
-  camera: {
-    fov: 75,
-    near: 0.1,
-    far: 1000,
-    position: { x: 0, y: 0, z: 2 },
-  },
-};
+import { CONFIG } from './config/app.config';
 
 // Canvas
 const getCanvas = () =>
