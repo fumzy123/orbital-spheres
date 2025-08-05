f78f19b767ac6f222a472b3cbc8630ddcbc85575
Fume Uvwie <25962091+fumzy123@users.noreply.github.com>
2025-08-05 00:58:51 -0230

    updated renderer

--- src/core/renderer.js
+++ src/core/renderer.js
@@ -1,9 +1,17 @@
-
 import * as THREE from 'three';
 import { CONFIG } from '../config/app.config';
 
-export function createRenderer (canvas) {
-    const renderer = new THREE.WebGLRenderer({canvas});
-    renderer.setSize(CONFIG.canvas.width, CONFIG.canvas.height);
-    return renderer;
+export function createRenderer(canvas) {
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setSize(CONFIG.canvas.width, CONFIG.canvas.height);
+  return renderer;
+}
+
+export function loopRenderer(renderer, scene, camera) {
+  const animate = () => {
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+  };
+
+  return animate;
 }
